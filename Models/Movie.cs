c063e8d425b0c070170318a6b66f616f76f7a0bb
Yasinvalipoor <yasinvalipoor14@gmail.com>
2025-01-26 08:48:06 +0330

    Add Data Validation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,14 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using BlazorMovieApp.Models.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorMovieApp.Models;
 
 public class Movie
 {
     public int Id { get; set; }
+
+
+    [Required(ErrorMessage = "This field is required.")]
     public string Title { get; set; } = string.Empty;
 
-    [DataType(DataType.Date)]
+
+    [Required]
+    [DataType(DataType.DateTime)]
+    [ReleaseDateValidation]
+    [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
     public DateTime ReleaseDate { get; set; }
+
+
+    [Required(ErrorMessage = "This field is required.")]
     public string Genre { get; set; } = string.Empty;
+
+
+    [Range(1, 100, ErrorMessage = "The value must be between 1 and 100.")]
     public decimal Price { get; set; }
 }
---
+++ Models/ValidationAttributes/ReleaseDateValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorMovieApp.Models.ValidationAttributes;
+
+public class ReleaseDateValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is DateTime dateValue)
+        {
+            var minDate = new DateTime(1980, 1, 1);
+            var maxDate = new DateTime(2100, 12, 31);
+
+            if (dateValue < minDate || dateValue > maxDate)
+            {
+                return new ValidationResult($"The date must be after {minDate.ToShortDateString()}");
+            }
+        }
+        return ValidationResult.Success;
+    }
+}
